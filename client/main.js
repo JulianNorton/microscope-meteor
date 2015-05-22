532d3ba42396c2a80bd17f006c9a141bf70ef417
j <julian@juliannorton.com>
2015-05-22 12:43:35 -0400

    very basic routing commit 5-1 p.66

--- client/main.js
+++ client/main.js
@@ -1,1 +1,2 @@
-// empty
+
+Meteor.subscribe('posts');
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,23 +1,5 @@
-var postsData = [
-  {
-    title: 'Introduction Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope'
-  },
-
-  {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com',
-  },
-
-  {
-    title: 'Test title',
-    author: 'Test author',
-    url: 'http://placehold.it',
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
