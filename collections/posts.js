e36908e95306a9cc82db835742e65e0c33dfcc60
j <julian@juliannorton.com>
2015-05-26 10:06:41 -0400

    commented double bang, alert copy

--- collections/posts.js
+++ collections/posts.js
@@ -4,6 +4,10 @@ Posts = new Meteor.Collection('posts');
 Posts.allow({
   insert: function(userId, doc) {
     // only allow posting if you're logged in
+    // '!!' converts a value to a boolean and ensures a boolean type.
+    // It's not returning the user ID, it's returning true/false on the user ID
+
+
     return !! userId;
   }
 });
